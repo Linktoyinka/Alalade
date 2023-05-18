dc0fed6bfd28cb579849096f006d34ae82c6963c
Afolabi <linktoyinka@gmail.com>
2023-05-18 18:13:05 +0100

    homepage mobile responsiveness

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 const main_menu = document.querySelector(".menu");
 const hamburger = document.querySelector(".hamburger");
 const mobile_menu = document.querySelector(".nav-panel");
+const body = document.querySelector(".body");
 
 main_menu.addEventListener("click", function () {
   hamburger.classList.toggle("is-active");
   mobile_menu.classList.toggle("is-active");
+  body.classList.toggle("scroll");
 });
