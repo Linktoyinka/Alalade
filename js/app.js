3fcb727838809693255e384b45c3d7bd0c6ec261
Afolabi <linktoyinka@gmail.com>
2023-06-20 00:54:12 +0100

    "About Me" button switch

--- js/app.js
+++ js/app.js
@@ -7,6 +7,29 @@ main_menu.addEventListener("click", function () {
   hamburger.classList.toggle("is-active");
   mobile_menu.classList.toggle("is-active");
   body.classList.toggle("scroll");
+
+
+  // About menu button switch
+    // Get the anchor element
+    const anchorElement = document.getElementById('cv');
+
+    // change background color
+    anchorElement.classList.toggle("btn-bg")
+  
+    // Toggle the href attribute
+    if (anchorElement.href = '../about.html') {
+      anchorElement.href = '../new-about.html';
+    } else {
+      anchorElement.href = '../about.html';
+    }
+  
+    // Toggle the text content
+    if (anchorElement.textContent === 'About Me') {
+      anchorElement.textContent = 'Download CV';
+    } else {
+      anchorElement.textContent = 'About Me';
+    }
+  
 });
 
 
@@ -14,3 +37,4 @@ window.addEventListener("scroll", function() {
   var header = document.querySelector(".header");
   header.classList.toggle("blur-header", window.scrollY > 50);
 });
+
